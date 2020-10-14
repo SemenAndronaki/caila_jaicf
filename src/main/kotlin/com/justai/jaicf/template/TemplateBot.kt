519e17e5b5d0c4d40e33ca6cbc364e7646bfac9e
SemenAndronaki <semenandronaki@gmail.com>
2020-10-14 13:00:14 +0300

    Initial commit

---
+++ src/main/kotlin/com/justai/jaicf/template/TemplateBot.kt
@@ -0,0 +1,29 @@
+package com.justai.jaicf.template
+
+import com.justai.jaicf.BotEngine
+import com.justai.jaicf.activator.caila.CailaIntentActivator
+import com.justai.jaicf.activator.caila.CailaNLUSettings
+import com.justai.jaicf.activator.regex.RegexActivator
+import com.justai.jaicf.channel.jaicp.logging.JaicpConversationLogger
+import com.justai.jaicf.template.scenario.MainScenario
+import java.util.*
+
+val accessToken: String = System.getenv("JAICP_API_TOKEN") ?: Properties().run {
+    load(CailaNLUSettings::class.java.getResourceAsStream("/jaicp.properties"))
+    getProperty("apiToken")
+}
+
+private val cailaNLUSettings = CailaNLUSettings(
+    accessToken = accessToken
+)
+
+val templateBot = BotEngine(
+    model = MainScenario.model,
+    conversationLoggers = arrayOf(
+        JaicpConversationLogger(accessToken)
+    ),
+    activators = arrayOf(
+        CailaIntentActivator.Factory(cailaNLUSettings),
+        RegexActivator
+    )
+)
